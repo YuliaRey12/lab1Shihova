caca707c7f30568bbeabf11ce08690cf478d56fa
YuliaRey12 <ostrovskaya118@yandex.ru>
2025-06-10 18:01:21 +0700

    реализована логика анализа возраста

--- app/src/main/java/com/example/lab1shihova/MainActivity.kt
+++ app/src/main/java/com/example/lab1shihova/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.lab1shihova
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val ageInput = findViewById<EditText>(R.id.ageInput)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
+        checkButton.setOnClickListener {
+            val age = ageInput.text.toString().toIntOrNull()
+            resultText.text = when {
+                age == null -> "Введите корректный возраст"
+                age <= 20 -> "Вы слишком молоды!"
+                age == 30 || age == 40 || age == 50 || age == 60 -> "Поздравляем с повышением!"
+                age == 65 -> "Преподносим вам золотые часы!"
+                age > 65 -> "Вы слишком стары!"
+                else -> "Продолжайте накапливать опыт!"
+            }
         }
     }
 }
